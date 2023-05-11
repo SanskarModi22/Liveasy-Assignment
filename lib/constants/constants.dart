324dc1cb396abf935d16668d3befd490a99b4f2a
Sanskar Modi <modisanskar5@gmail.com>
2023-05-11 23:11:44 +0530

    Home Screen Started

---
+++ lib/constants/constants.dart
@@ -0,0 +1,9 @@
+class Constants {
+  static const galleryImg = "assets/gallery_img.png";
+  static const indiaFlag = "assets/india_flag.png";
+  static const page1Vector = "assets/pg1_bottom.png";
+  static const page2Vector = "assets/pg2_bottom.png";
+  static const page3Vector = "assets/pg3_bottom.png";
+  static const shipperImg = "assets/shipper_img.png";
+  static const transporterImg = "assets/transporter_img.png";
+}
