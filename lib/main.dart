1f4c5bdb85c4c0336a62ae761e21e5440c133bca
Sanskar Modi <modisanskar5@gmail.com>
2023-05-14 19:37:45 +0530

    Firebase Added in the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:liveasy_assignment/views/profile_screen.dart';
 
-void main() => runApp(const MyApp());
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Awesome Credit Card Demo',
+      title: 'Liveasy',
       theme: ThemeData(
         fontFamily: 'Roboto',
       ),
